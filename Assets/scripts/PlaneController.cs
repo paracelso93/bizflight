ebf21646a624a566f95ceb714ead0ee01502ddf2
paracelso93 <68126328+paracelso93@users.noreply.github.com>
2021-04-07 15:06:55 +0200

    obamide

--- Assets/scripts/PlaneController.cs
+++ Assets/scripts/PlaneController.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaneController : MonoBehaviour
 {
-
+    public Slider slider;
     public float speed = 100.0f;
+    public float rotation_speed = 10.0f;
+    Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
+        slider.minValue = 0.0f;
+        slider.maxValue = 40.0f;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float pitch = -Input.GetAxis("Vertical") * speed;
-        float roll = Input.GetAxis("Horizontal") * speed;
-        //Quaternion rotation = transform.rotation;
-        //Vector3 euler = rotation.eulerAngles;
-        transform.Rotate(pitch, 0, roll, Space.World);
-        transform.Translate(transform.forward * Time.deltaTime * speed);
+        float pitch = Input.GetAxis("Vertical") * rotation_speed * Time.deltaTime; // S W
+        float roll = -Input.GetAxis("Roll") * rotation_speed * Time.deltaTime; // Q E
+        float yaw = Input.GetAxis("Horizontal") * rotation_speed * Time.deltaTime; // A D
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            speed += 0.5f;
+            speed = Mathf.Clamp(speed, 0.0f, 40.0f);
+            slider.value = speed;
+        }
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            speed -= 0.5f;
+            speed = Mathf.Clamp(speed, 0.0f, 40.0f);
+            slider.value = speed;
+        }
+        
+        //transform.Rotate(pitch, yaw, roll, Space.World);
+        //rigidbody.rotation = Quaternion.Euler(pitch * 100, yaw * 100, roll * 100);
+        transform.Rotate(new Vector3(pitch, yaw, roll));
+        rigidbody.velocity = transform.forward * speed;
     }
 }
