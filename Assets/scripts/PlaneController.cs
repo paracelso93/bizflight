cecf76a0b303fd137b1798b82bfd9213baaf5ff5
paracelso93 <68126328+paracelso93@users.noreply.github.com>
2021-04-07 13:21:18 +0200

    push

---
+++ Assets/scripts/PlaneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+
+    public float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pitch = -Input.GetAxis("Vertical") * speed;
+        float roll = Input.GetAxis("Horizontal") * speed;
+        //Quaternion rotation = transform.rotation;
+        //Vector3 euler = rotation.eulerAngles;
+        transform.Rotate(pitch, 0, roll, Space.World);
+        transform.Translate(transform.forward * Time.deltaTime * speed);
+    }
+}
